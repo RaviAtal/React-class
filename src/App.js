391ffab2ab6b52995875d6b0e0714ebb8feaa0e5
RaviAtal <raviatal44@gmail.com>
2023-03-23 14:46:48 +0530

    rough_01

--- src/App.js
+++ src/App.js
@@ -2,24 +2,16 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle";
-import Usestat17 from "./components/Usestat17";
-import Rough from "./components/Rough";
-import { UseOfCards } from "./components/UseOfCards";
 import { About } from "./components/About";
 import Main from "./components/Main";
 
 function App() {
   return (
     <>
-      {/* ROUGHT of usestate 17-03 */}
-      {/* <Rough /> */}
-      {/* use of Map in Cards 21-03*/}
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="about" element={<About />} />
       </Routes>
-      {/* <UseOfCards /> */}
-      {/* <About /> */}
     </>
   );
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { UseOfCards } from "./UseOfCards";
 import Usestat17 from "./Usestat17";
+import Rough from "./components/Rough";
 
 function Main() {
   return (
     <>
+      {/* ROUGHT of usestate 17-03 */}
+      {/* <Rough /> */}
       <Usestat17 />
+      {/* use of Map in Cards 21-03 */}
       <UseOfCards />
     </>
   );
